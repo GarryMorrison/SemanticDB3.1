f9d438544fe0715ce3422f05e7ac1a157c4853f0
Garry Morrison <garry@semantic-db.org>
2020-11-14 12:18:45 +1030

    TCLAP successfully compiles, now it is time to make use of it.

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,17 @@
 #include <memory>
 
 #include "SDB3_header.h"
+#include "tclap/CmdLine.h"
+
+
+std::string introduction_text = "Welcome to the SDB 3.1! Last updated 29th October 2020. Type h for help.\n";
 
 
 int main() {
 
     ContextList context("Global context");
     SDB::Driver driver(context);
-
+    std::cout << introduction_text;
 
     return 0;
 }
