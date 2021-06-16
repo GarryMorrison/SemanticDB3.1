b5fd22ec1bb24d1c356f62b335e1ed40830d8766
Garry Morrison <garry@semantic-db.org>
2021-06-16 11:26:23 +0930

    Bound functions now store and then restore variables that are stomped on by the bound step.

--- src/Operator/FunctionOperator.cpp
+++ src/Operator/FunctionOperator.cpp
@@ -121,19 +121,65 @@ Sequence FunctionOperator::Compile(ContextList& context, const Sequence& seq) co
         // std::cout << "found a bound function, size: " << seq_vec.size() << "\n";
         ulong input_idx = ket_map.get_idx("input");  // learn input|seq>. A little heavier and slower than I would like!
         ulong seq_idx = ket_map.get_idx("seq");      // Also, we have hard wired in the op/ket labels here. Is there a better way?
-        context.unlearn(input_idx, seq_idx);
+        // context.unlearn(input_idx, seq_idx);         // Not needed now we can learn empty sequences |>.
+
+        std::shared_ptr<BaseSequence> bSeq_empty = std::make_shared<Ket>();  // Learn variables before we stomp on them.
+        std::shared_ptr<BaseSequence> bSeq_tmp;
+        std::vector<unsigned int> stored_type;
+        std::vector<std::shared_ptr<BaseSequence> > stored_bseq;
+        unsigned int rule_type = context.recall_type(input_idx, seq_idx);
+        stored_type.push_back(rule_type);
+        if (rule_type == RULEUNDEFINED) {
+            stored_bseq.push_back(bSeq_empty);
+        } else {
+            bSeq_tmp = context.recall(input_idx, seq_idx);
+            stored_bseq.push_back(bSeq_tmp);
+        }
         std::shared_ptr<BaseSequence> bSeq = std::make_shared<Sequence>(args[0]);
         context.learn(input_idx, seq_idx, bSeq);
         std::vector<ulong> params = context.bound_fn_params_recall(idx, seq_vec.size());
         for (unsigned int i = 0; i < params.size(); i += 2) {
             ulong op_idx = params[i];
             ulong ket_idx = params[i + 1];
-            std::shared_ptr<BaseSequence> bSeq = std::make_shared<Sequence>(args[(i/2) + 1]);
-            context.unlearn(op_idx, ket_idx);           // A little bit expensive, but I think we need it if |> is fed as a parameter to our functions.
-            context.learn(op_idx, ket_idx, bSeq);  // In which case, the learn() will be a no-op, meaning previous values will be used. Which we don't want!
+            bSeq = std::make_shared<Sequence>(args[(i/2) + 1]);
+            // context.unlearn(op_idx, ket_idx);           // A little bit expensive, but I think we need it if |> is fed as a parameter to our functions.
+            // context.learn(op_idx, ket_idx, bSeq);  // In which case, the learn() will be a no-op, meaning previous values will be used. Which we don't want!
+            rule_type = context.recall_type(op_idx, ket_idx);
+            stored_type.push_back(rule_type);
+            if (rule_type == RULEUNDEFINED) {
+                stored_bseq.push_back(bSeq_empty);
+            } else {
+                bSeq_tmp = context.recall(op_idx, ket_idx);
+                stored_bseq.push_back(bSeq_tmp);
+            }
+            context.learn(op_idx, ket_idx, bSeq);
         }
         auto rule = context.bound_fn_body_recall(idx, seq_vec.size());
-        return rule->Compile(context, empty_ket, args);
+        // return rule->Compile(context, empty_ket, args);
+        Sequence result = rule->Compile(context, empty_ket, args);
+
+        // Now restore the original variables:
+        rule_type = stored_type[0];
+        switch (rule_type) {
+            case RULENORMAL: context.learn(input_idx, seq_idx, stored_bseq[0]); break;
+            case RULESTORED: context.stored_learn(input_idx, seq_idx, stored_bseq[0]); break;
+            case RULEMEMOIZE: context.memoize_learn(input_idx, seq_idx, stored_bseq[0]); break;
+            case RULEUNDEFINED: context.unlearn(input_idx, seq_idx); break;
+            default: break;
+        }
+        for (unsigned int i = 0; i < params.size(); i += 2) {
+            ulong op_idx = params[i];
+            ulong ket_idx = params[i + 1];
+            rule_type = stored_type[i/2 + 1];
+            switch (rule_type) {
+                case RULENORMAL: context.learn(op_idx, ket_idx, stored_bseq[i/2 + 1]); break;
+                case RULESTORED: context.stored_learn(op_idx, ket_idx, stored_bseq[i/2 + 1]); break;
+                case RULEMEMOIZE: context.memoize_learn(op_idx, ket_idx, stored_bseq[i/2 + 1]); break;
+                case RULEUNDEFINED: context.unlearn(op_idx, ket_idx); break;
+                default: break;
+            }
+        }
+        return result;
     }
     return Sequence();
 }
